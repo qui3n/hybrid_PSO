f36ee28c43a1955f96f5c294d85afdf5359ce0fd
ReaCaer <caerdf@hotmail.com>
2014-10-03 22:07:41 +0200

    benchmark di tutte le funzioni

--- hybrid_PSO/hybrid_PSO.cpp
+++ hybrid_PSO/hybrid_PSO.cpp
@@ -1,24 +1,36 @@
 #include "stdafx.h"
 #include "Swarm.h"
+#include <windows.h>
 
 int _tmain(int argc, _TCHAR* argv[])
-{
+{		
 	Swarm swarm;
+	int iter = 1;
+	double bestFitness[28] = {0};
+	int successNumber[28] = {0};
 
-	int num = 0;
-	double bestFitness[28];
+	while(1)
+	{
+		int num = 0;
+		while(num <= 27)
+		{
+			swarm.functionNumber = num+1;
+			swarm.run();
+			if(swarm.swarmBestFitness < 200)
+			{
+				bestFitness[num] += swarm.swarmBestFitness;
+				successNumber[num]++;
+			}
+			num++;
+		}
+		system("CLS");
+		std::cout << "iteration " << iter << ":\tsuccess\taverage \n";
+		for(int i=0; i<28; i++)
+		{
+			std::cout << "function " << i+1 << ": \t(" << successNumber[i] << ")\t" << ( successNumber[i] != 0 ? (bestFitness[i] / successNumber[i]) : -1 ) << "\n";
+		}
 
-	while(num <= 27)
-	{
-		swarm.run();
-		bestFitness[num] = swarm.swarmBestFitness;
-		num++;
+		iter++;
 	}
-	for(int i=0; i<28; i++)
-	{
-		std::cout << "function " << i+1 << ": \t" << bestFitness[i] << "\n";
-	}
-	while(1);
 	return 0;
 }
-
