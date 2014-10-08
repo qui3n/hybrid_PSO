7b4e95dafa14c007845fb81abd8916fff1c8d64e
ReaCaer <caerdf@hotmail.com>
2014-10-08 17:19:34 +0200

    .

--- hybrid_PSO/hybrid_PSO.cpp
+++ hybrid_PSO/hybrid_PSO.cpp
@@ -8,30 +8,14 @@ int _tmain(int argc, _TCHAR* argv[])
 {		
 	Swarm swarm;
 	int iter = 0;
-	int const maxIter = 500;
+	int const maxIter = 5;
 	double bestFitness[28] = {0};
 	int successNumber[28] = {0};
 
-	std::time_t rawtime;
-    std::tm* timeinfo;
-    char buffer [80];
-
-    std::time(&rawtime);
-    timeinfo = std::localtime(&rawtime);
-	std::strftime(buffer,80,"%Y_%m_%d-%H_%M_%S",timeinfo);
-    std::puts(buffer);
-
-	std::ofstream log;
-	std::string filename = "logs_", directory = "benchmark logs/";
-	filename += buffer;
-	filename += "_HPSOv2";
-	filename += ".txt";
-	log.open(directory + filename);
-
 	for(int k=0; k<maxIter; ++iter, k++)
 	{
-		int num = 0;
-		while(num <= 27)
+		iter++;
+		for(int num=0; num<28; num++)
 		{
 			swarm.functionNumber = num+1;
 			swarm.run();
@@ -40,7 +24,6 @@ int _tmain(int argc, _TCHAR* argv[])
 				bestFitness[num] += swarm.swarmBestFitness;
 				successNumber[num]++;
 			}
-			num++;
 		}
 		system("CLS");
 		std::cout << "iteration " << iter << ":\tsuccess\taverage \n";
@@ -49,6 +32,24 @@ int _tmain(int argc, _TCHAR* argv[])
 			std::cout << "function " << i+1 << ": \t(" << successNumber[i] << ")\t" << ( successNumber[i] != 0 ? (bestFitness[i] / successNumber[i]) : -1 ) << "\n";
 		}
 	}
+
+	// log
+	std::time_t rawtime;
+	std::tm* timeinfo;
+	char buffer [80];
+
+	std::time(&rawtime);
+	timeinfo = std::localtime(&rawtime);
+	std::strftime(buffer,80,"%Y_%m_%d-%H_%M_%S",timeinfo);
+	std::puts(buffer);
+
+	std::ofstream log;
+	std::string filename = "logs_", directory = "logs/";
+	filename += buffer;
+	filename += "_HPSOv2_";
+	filename += std::to_string(static_cast<unsigned long long>(maxIter));
+	filename += ".txt";
+	log.open(directory + filename);
 
 	log << "iteration " << iter << ":\tsuccess\taverage \n";
 	for(int i=0; i<28; i++)
